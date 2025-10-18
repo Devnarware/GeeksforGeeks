5b6f03be539d659b5eef0129c2c0baaf60457f39
Devendra Kumar Narware <devn2405@gmail.com>
2025-10-18 23:36:55 +0530

    Optimize leaders method for better performance
    Refactor leaders method to improve efficiency and readability.

--- Array_Leaders.java
+++ Array_Leaders.java
@@ -1,24 +1,45 @@
+// class Solution {
+//     static int max(int[] arr, int n){
+//         int max = n ;
+//         for(int i = n ; i<arr.length ; i++ ){
+//             if(arr[max]<arr[i]){
+//                 max = i ;
+//             }
+//         }
+//         return max ;
+//     }
+//     static ArrayList<Integer> leaders(int arr[]) {
+//         ArrayList<Integer> list = new ArrayList<>() ;
+//         int count = 0 ;
+//         while(count<arr.length-1){
+//             int i = max(arr, count) ;
+//             list.add(arr[i]) ;
+//             count = i+1 ;
+//         }
+//         if (count<arr.length){
+//             list.add(arr[arr.length-1]) ;
+//         }
+//         return list ;
+//     }
+// }
+
+//UPDATED VERSION
+
+
 class Solution {
-    static int max(int[] arr, int n){
-        int max = n ;
-        for(int i = n ; i<arr.length ; i++ ){
-            if(arr[max]<arr[i]){
-                max = i ;
+    static ArrayList<Integer> leaders(int arr[]) {
+        // code here
+        ArrayList<Integer> list = new ArrayList<Integer>() ;
+        list.add(arr[arr.length-1]) ;
+        int max = arr[arr.length-1] ;
+        for(int i = arr.length-2 ; i>=0 ;i--){
+            if(arr[i]>=max){
+                list.add(arr[i]) ;
+                max = arr[i] ;
             }
         }
-        return max ;
-    }
-    static ArrayList<Integer> leaders(int arr[]) {
-        ArrayList<Integer> list = new ArrayList<>() ;
-        int count = 0 ;
-        while(count<arr.length-1){
-            int i = max(arr, count) ;
-            list.add(arr[i]) ;
-            count = i+1 ;
-        }
-        if (count<arr.length){
-            list.add(arr[arr.length-1]) ;
-        }
+        Collections.reverse(list);
         return list ;
     }
 }
+
